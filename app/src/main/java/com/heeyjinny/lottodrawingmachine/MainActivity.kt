8eb1a4056200c275fb3872a544e296b44d511e17
heeyjinny <heeyjinny@gmail.com>
2022-12-31 04:00:50 +0900

    Fin.

--- app/src/main/java/com/heeyjinny/lottodrawingmachine/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/lottodrawingmachine/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 
     //2
     //넘버피커 연결
-    private  val numPicker: NumberPicker by lazy {
+    private val numPicker: NumberPicker by lazy {
         findViewById(R.id.numPicker)
     }
 
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 
     //3
     //btnRun버튼을 클릭했을때 설정되는 함수 생성
-    private fun initBtnRun(){
+    private fun initBtnRun() {
         //3-1
         //btnRun버튼 클릭 시 실행
         btnRun.setOnClickListener {
@@ -86,7 +86,7 @@ class MainActivity : AppCompatActivity() {
             //작성된 로또번호 리스트를 텍스트뷰에 띄우기
             //forEach{}문을 사용하면 몇 번째의 인덱스 값이 넘어오는 지 모름
             //forEachIndexed{} 사용하여 인덱스 값과 리스트의값(넘버값)을 가져와 사용
-            list.forEachIndexed{ index, number ->
+            list.forEachIndexed { index, number ->
                 //8-1
                 //텍스트뷰 리스트에 저장된 현재 인덱스 값 저장
                 val textView = numTextViewList[index]
@@ -108,7 +108,7 @@ class MainActivity : AppCompatActivity() {
 
     //5
     //btnAdd버튼을 클릭했을 때 설정되는 함수 생성
-    private fun initBtnAdd(){
+    private fun initBtnAdd() {
         btnAdd.setOnClickListener {
             //5-1
             //버튼을 클릭했을 때 예외 3가지 처리
@@ -116,7 +116,7 @@ class MainActivity : AppCompatActivity() {
             //이미 btnRun버튼을 클릭했을 때 초기화하라는 알림 설정
             //전역변수로 didRun생성하여 false값 저장하여 사용
             //didRun의 값이 true가 되어있다면 알림 설정
-            if(didRun){
+            if (didRun) {
                 Toast.makeText(this, "초기화 후에 시도해주세요.", Toast.LENGTH_SHORT).show()
                 //다시 클릭리스너 메서드 실행하기
                 return@setOnClickListener
@@ -124,14 +124,14 @@ class MainActivity : AppCompatActivity() {
             //5-1-2
             //선택할 수 있는 번호는 최대 5개가 될 수 있다는 알림 설정
             //전역변수로 numPickerSet 생성하여 사용
-            if (numPickerSet.size >= 5){
+            if (numPickerSet.size >= 5) {
                 Toast.makeText(this, "번호는 5개까지 선택할 수 있습니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             //5-1-3
             //넘버피커에서 선택한 숫자가 중복된 값이면 알림 설정
             //전역변수 numPickerSet사용하여 리스트에 중복된 값이 존재하면 추가되지 않고 알림 설정
-            if (numPickerSet.contains(numPicker.value)){
+            if (numPickerSet.contains(numPicker.value)) {
                 Toast.makeText(this, "이미 선택한 번호입니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
@@ -165,23 +165,27 @@ class MainActivity : AppCompatActivity() {
     //when문을 사용하여 텍스트뷰의 각 값의 범위에 따라 백그라운드 설정
     //btnAdd와 btnRun에도 필요하기 때문에 함수를 만들어 사용
     //파라미터로 숫자와 보여주는 뷰인 텍스트뷰를 받는 함수
-    private fun setNumBackground(number: Int, textView: TextView){
+    private fun setNumBackground(number: Int, textView: TextView) {
         //9-1
         //번호에 맞게 공의 색상 변경되도록 설정
         //drawable에 저장되어있는 도형들은 앱에 저장되어 있기 때문에
         //ContextCompat을 사용해 가져옴
-        when(number){
-            in 1..10 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
-            in 11..20 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
-            in 21..30 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
-            in 31..40 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+        when (number) {
+            in 1..10 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_gray)
             else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
         }
     }//setNumBackground
 
     //6
     //초기화버튼 코드 작성
-    private fun initBtnClear(){
+    private fun initBtnClear() {
         btnClear.setOnClickListener {
             //6-1
             //피커set리스트 값을 모두 삭제
@@ -200,19 +204,19 @@ class MainActivity : AppCompatActivity() {
     //4
     //번호를 가지고 있는 리스트 형식의 랜덤번호 생성 함수 작성
     //fun getRandomNum(): List<Int>{}
-    private fun getRandomNum(): List<Int>{
+    private fun getRandomNum(): List<Int> {
 
         //4-1
         //리스트 넘버를 뮤터블리스트로 만들고 mutableListOf<Int>()
         //1~45의 숫자를 차례대로 추가.add()한 리스트로 초기화 .apply{}
         val numList = mutableListOf<Int>().apply {
-            for (i in 1..45){
+            for (i in 1..45) {
                 //7
                 //조건식 추가
                 //이미 선택되어 있는 번호는 랜덤으로 돌리지 않기 위해
                 //만약 numPickSet의 값 중 이미 i의 값이 있을 경우
                 //continue하여 넘버 리스트에 추가하지 않고 다시 반복문 실행
-                if (numPickerSet.contains(i)){
+                if (numPickerSet.contains(i)) {
                     continue
                 }
                 this.add(i)
